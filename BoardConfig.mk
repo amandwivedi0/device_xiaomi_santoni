15c7211d00a4ddf4cce2f2495bdaad1cbcc3b435
Ali Hasan <ahb7671@gmail.com>
2020-02-19 14:32:48 +0500

    santoni: import power hal from LA.UM.8.6.r1-03400-89xx.0
    Signed-off-by: Ali Hasan <ahb7671@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -211,6 +211,8 @@ BLOCK_BASED_OTA := true
 TARGET_PER_MGR_ENABLED := true
 
 # Power
+TARGET_PROVIDES_POWERHAL := true
+TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/enable_dt2w"
 TARGET_USES_INTERACTION_BOOST := true
 
 # Qualcomm
